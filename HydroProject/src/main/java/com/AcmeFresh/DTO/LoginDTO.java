d5dd08b72d33ad27163d7179a47cc0492fadf8be
roshan93190 <roshan93190>
2022-11-10 17:10:36 +0530

    DTO added

---
+++ HydroProject/src/main/java/com/AcmeFresh/DTO/LoginDTO.java
@@ -0,0 +1,11 @@
+package com.AcmeFresh.DTO;
+
+import lombok.Data;
+
+@Data
+public class LoginDTO {
+	private String username;
+	private String mobile;
+	private String password;
+
+}
---
+++ HydroProject/src/main/java/com/AcmeFresh/DTO/RegisteringDTO.java
@@ -0,0 +1,18 @@
+package com.AcmeFresh.DTO;
+
+import lombok.Data;
+
+@Data
+public class RegisteringDTO {
+	
+	private String username;
+
+	private String password;
+	
+	private String address;
+	
+	private String mobile;
+	
+	private String email;
+
+}
